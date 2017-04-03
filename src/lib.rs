2a89b62746d0337ff74b24a773c7614da9041db4
Amos Onn <amosonn@gmail.com>
2017-04-03 23:07:58 +0200

    Crafted lib exports.

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,37 @@ mod fusefl;
 mod handler_table;
 mod filelike;
 
-pub use fusefl::*;
+pub use fusefl::{
+    CreatedEntryObj,
+    ResultOpenObj,
+    ResultCreateObj,
+    FilesystemFL,
+    FuseFL,
+};
+pub use filelike::{
+    ReadFileLike,
+    WriteFileLike,
+    NoFile,
+    ReadWriteAdaptor,
+    ModalFileLike,
+    FilesystemFLRwOpen,
+    FilesystemFLOpen,
+};
+pub use fuse_mt::{
+    RequestInfo,
+    DirectoryEntry,
+    Statfs,
+    Xattr,
+    ResultEmpty,
+    ResultGetattr,
+    ResultEntry,
+    ResultReaddir,
+    ResultData,
+    ResultWrite,
+    ResultStatfs,
+    ResultXattr,
+    FuseMT,
+};
 
 use std::result;
 pub type Result<T> = result::Result<T, libc::c_int>;
